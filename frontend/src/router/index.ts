91ed8ebdde7038444a738f53a39836687f2577f1
Thimofej <thimo@timax.io>
2026-03-09 05:18:20 +0100

    feat: US-051 - Blacklist management page UI

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -89,6 +89,12 @@ const routes: RouteRecordRaw[] = [
         meta: { roles: ['admin', 'main_manager', 'rental_manager', 'office_worker', 'customer_service'] },
       },
       {
+        path: 'customers/blacklist',
+        name: 'Blacklist',
+        component: () => import('../views/BlacklistView.vue'),
+        meta: { roles: ['admin', 'main_manager', 'rental_manager', 'office_worker', 'customer_service'] },
+      },
+      {
         path: 'customers/:id',
         name: 'CustomerDetail',
         component: () => import('../views/CustomerDetailView.vue'),
